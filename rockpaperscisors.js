5f56b4e45ba15d1b82e7d22e42ec47e8cc87607a
sebik9 <s3bik9@seznam.cz>
2023-10-18 11:50:48 +0200

    Create rockpaperscisors.js
    upload console version of rock paper scisors in JS

---
+++ rockpaperscisors.js
@@ -0,0 +1,67 @@
+//deklarace funkce prijima parametr userInput a porovnává jestli je sprivně zadaná hodnota 
+const getUserChoice = userInput => {
+  userInput = userInput.toLowerCase();
+  if (userInput === "kámen" || userInput === "nůžky" || userInput === "papír" || userInput === 'bomba') {
+    return userInput;
+  } else {
+    console.log("zadaná hodnota musí být kámen, nůžky nebo papír");
+  }
+};
+
+//funkce co generuje výběr PC za pomocí náhodného čísla a case switch
+const getComputerChoice = () => {
+  const randomNum = Math.floor(Math.random() * 3);
+  switch (randomNum) {
+    case 0:
+      return "kámen";
+      break;
+    case 1:
+      return "nůžky";
+      break;
+    case 2:
+      return "papír";
+      break;
+  }
+};
+
+//funkce která přijímá funkce jako parametr a porovnává hodnoty
+const determineWinner = (userChoice, computerChoice) => {
+  if (userChoice === 'bomba') {
+    return 'Vyhrál hráč'
+  }
+  if (userChoice === computerChoice) {
+    return "remíza";
+  }
+  if (userChoice === "kámen") {
+    if (computerChoice === "papír") {
+      return "vyhrál počítač";
+    } else {
+      return "Vyhrál hráč";
+    }
+  }
+  if (userChoice === "papír") {
+    if (computerChoice === "nůžky") {
+      return "Vyhrál počítač";
+    } else {
+      return "Vyhrál hráč";
+    }
+  }
+  if (userChoice === "nůžky") {
+    if (computerChoice === "kámen") {
+      return "počítač vyhrál";
+    } else {
+      return "vyhrál hráč";
+    }
+  }
+};
+
+//funkce která spustí hru
+const playGame = () => {
+  const userChoice = getUserChoice("bomba");
+  const computerChoice = getComputerChoice();
+  console.log("dal jsi " + userChoice);
+  console.log("počítač dal " + computerChoice);
+  console.log(determineWinner(userChoice, computerChoice));
+};
+
+playGame();
